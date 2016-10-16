c9ff12a179d622a91011479f1e58704cab849b59
Iatus <geovannimartins@ic.ufal.br>
2016-10-16 15:03:27 -0300

    Encrypt #1

--- Model/libs/librsa/inc/Decrypt.hpp
+++ Model/libs/librsa/inc/Decrypt.hpp
@@ -1,16 +1,26 @@
 #ifndef DECRYPT_HPP
 #define DECRYPT_HPP
 
+#include <fstream>
+#include <string>
 #include "Math2.hpp"
+
+typedef unsigned char uchar;
+typedef unsigned long long int ulong_64;
+
 class Decrypt {
 public:
-	Encrypt(string input, string output, unsigned long long key1 , unsigned long long key2);
+	Decrypt(std::string input, std::string output, 
+	             ulong_64 key1 , ulong_64 key2);
 private:
 	void decrypt();
-	unsigned long long int m_ring;
-	unsigned long long int m_e;
-	unsigned long long int m_d;
-	
+	ulong_64 m_ring;
+	ulong_64 m_e;
+	ulong_64 m_d;
+	std::ifstream m_input;
+	std::ofstream m_output;
+	std::string m_path_input;
+	std::string m_path_output;
 };
 
 #endif // !DECRYPT_HPP
--- Model/libs/librsa/src/Decrypt.cpp
+++ Model/libs/librsa/src/Decrypt.cpp
@@ -1,26 +1,38 @@
 #include "../inc/Decrypt.hpp"
 
 
-Decrypt::Decrypt(string input, string output, unsigned long long key1 , unsigned long long key2)
+Decrypt::Decrypt(std::string input, std::string output, 
+                      ulong_64 key1 , ulong_64 key2)
 {
+  m_path_input = input;
+
+  //Pick FilePath
+  std::size_t dash = input.find_last_of("/\\");
+  if(output == "")
+        m_path_output = input.substr(0,dash);
+  m_path_output += "/" + input.substr(dash+1);
+  
+  //Pick FileName
+  std::size_t dot  = m_path_output.find_last_of(".");
+  m_path_output = m_path_output.substr(0, dot);
+  m_path_output += ".huff";
+
 	m_e = key1;
 	m_d = key2;
 	m_ring = m_e * m_d;
 	/* Leitura
 		leitura do .agc
 	*/
+  m_input.open(m_path_input);
 	
 }
 
-void Decrypt::decrypt()
-{
-  fstream input;
-  // 
-  //input.open("../AlGi_Crypt0r/assets/primes.txt", fstream::in);
+void Decrypt::decrypt() {
+  std::ifstream input("../../../../Assets/primes.txt");
   ulong_64 v = 0;
   char c;
-  // saida do arquivo anterior
-  ofstream file(m_path_output);
+
+  std::ofstream file(m_path_output);
   while (input.get(c)) {
     if( c < '0' || c > '9' ) {
       file << (uchar)( Math2::inv(m_d,m_ring) );  
