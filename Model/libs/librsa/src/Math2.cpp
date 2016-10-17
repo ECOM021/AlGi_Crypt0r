3529fc6455ea7623c02fa950ac8212fef70dd858
alfredlima <alfredolms@ic.ufal.br>
2016-10-17 01:05:47 -0200

    FIX27

--- Model/libs/librsa/src/Math2.cpp
+++ Model/libs/librsa/src/Math2.cpp
@@ -4,23 +4,26 @@ Math2::Math2() {
 }
 
 ulong_64 Math2::exp(ulong_64 base, ulong_64 _exp, ulong_64 mod) {
-  if( _exp == 1 )
-    return base%mod;
-  else {
-    ulong_64 v = exp(base,_exp>>1,mod);
-    v = (v*v)%mod;
-    if( _exp&1 )
-      return (base*v)%mod;
-    return v;
+   ulong_64 pot = base % mod;
+   ulong_64 resultado = 1;
+
+  for ( ; _exp > 0; _exp /= 2) {
+    if (_exp&1)  /* O bit menos significativo e 1 */
+      resultado = (resultado * pot) % mod;
+    pot = (pot * pot) % mod;
   }
+  return resultado;
 }
+
 ulong_64 Math2::inv(ulong_64 value, ulong_64 mod) {
   return exp(value, mod-1, mod);
 }
+
 ulong_64 Math2::gcd( ulong_64 a, ulong_64 b ) {
 	if( !b ) return a;
 	return gcd( b, a%b );
 }
+
 ulong_64 Math2::sqrt(ulong_64 v) {
     if (v == 0 || v == 1)
        return v;
